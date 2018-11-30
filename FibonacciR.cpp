3f34a1bd88e80e9826fb1469a8d063b783a32283
direzzerOSU <40528290+direzzerOSU@users.noreply.github.com>
2018-11-29 18:52:37 -0800

    started assignment

---
+++ FibonacciR.cpp
@@ -0,0 +1,35 @@
+/*
+* FibonacciClass1.cpp
+* https://www.codeproject.com/tips/109443/fibonacci-recursive-and-non-recursive-c
+*
+*  Created on: Sep 10, 2010
+*      Author: julian
+*/
+
+  #include "FibonacciR.h"
+  #include <iostream>
+
+  FibonacciR::FibonacciR() {
+
+  }
+
+  FibonacciR::FibonacciR(const int &n) :n_(&n) {
+
+  }
+
+  FibonacciR::~FibonacciR() {
+
+  }
+
+  int FibonacciR::Fibonacci(const int &n) {
+  	if (n == 0)
+  		return 0;
+  	else if (n == 1)
+  		return 1;
+  	return Fibonacci(n - 1) + Fibonacci(n - 2);
+  }
+
+  void FibonacciR::PrintFibonacci() {
+  	int FibonaciNum = Fibonacci(*n_);
+  	std::cout << *n_ << "th fibonaci number: " << FibonaciNum << std::endl;
+  }
---
+++ recursion.cpp
@@ -0,0 +1,7 @@
+/*********************************************************************
+** Program name: Recursion vs. Iteration Efficiency
+** Author: Ryan DiRezze
+** Date: December 2, 2018
+** Description: 
+*********************************************************************/
+
